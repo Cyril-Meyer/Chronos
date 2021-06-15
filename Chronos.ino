0e7784ab222884b6aad734852dea8ebced02fd27
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-06-15 17:19:10 +0200

    minor upgrade, still work in progress

--- Chronos.ino
+++ Chronos.ino
@@ -21,7 +21,7 @@ SSD1306AsciiWire oled;
 long p1 = 0;  // time left for player 1 (ms)
 long p2 = 0;  // time left for player 2 (ms)
 long inc = 0; // time increment (ms)
-int mode = 0;          // 0 = menu, 1 = p1, 2 = p2, 3 = p1win, 4 = p2win, 5 = stand_by
+int mode = 0;          // 0 = menu, 1 = p1, 2 = p2, 3 = p1win, 4 = p2win, 5 = stand-by
 int btn_state = 0;     // 0 = nothing, 1 = p1, 2 = p2, 3 = both
 char L[4][11];
 unsigned long t0;
@@ -58,55 +58,114 @@ void loop()
 {
   t0 = millis();
   readBtnState();
-  
+
+  // menu
   if (mode == 0)
   {
     gameMenu();
   }
+  // play
   else if (mode < 3)
   {
     setDisplayLine(0, "P1      P2");
     setDisplayLine(1, "          ");
-    if (mode == 1)
+    
+    if (btn_state == 3)
     {
-      setDisplayLine(2, longMsToString(p1, false));
-      setDisplayLine(3, longMsToString(p2, true));
-      p1 = p1 - DELAY;
+      mode = 5;
+      digitalWrite(P1_LED_PIN, HIGH);
+      digitalWrite(P2_LED_PIN, HIGH);
     }
-    else if (mode == 2)
+    else
     {
-      setDisplayLine(3, longMsToString(p1, false));
-      setDisplayLine(2, longMsToString(p2, true));
-      p2 = p2 - DELAY;
+      if (mode == 1)
+      {
+        if (btn_state == 1)
+        {
+          mode = 2;
+          digitalWrite(P1_LED_PIN, LOW);
+          digitalWrite(P2_LED_PIN, HIGH);
+          p1 = p1 + inc;
+        }
+        else
+        {
+          setDisplayLine(2, longMsToString(p1, false));
+          setDisplayLine(3, longMsToString(p2, true));
+          p1 = p1 - DELAY;
+          
+          if (p1 <= 0)
+            mode = 4;
+        }
+      }
+      else if (mode == 2)
+      {
+        if (btn_state == 2)
+        {
+          mode = 1;
+          digitalWrite(P1_LED_PIN, HIGH);
+          digitalWrite(P2_LED_PIN, LOW);
+          p2 = p2 + inc;
+        }
+        else
+        {
+          setDisplayLine(3, longMsToString(p1, false));
+          setDisplayLine(2, longMsToString(p2, true));
+          p2 = p2 - DELAY;
+          
+          if (p2 <= 0)
+            mode = 3;
+        }
+      }
     }
   }
+  // end / stand-by
   else
   {
     if (mode == 5)
     {
-      mode = 1;
+      setDisplayLine(1, " STAND BY ");
+      if (btn_state == 1)
+      {
+        mode = 2;
+        digitalWrite(P1_LED_PIN, LOW);
+        digitalWrite(P2_LED_PIN, HIGH);
+      }
+      else if (btn_state == 2)
+      {
+        mode = 1;
+        digitalWrite(P1_LED_PIN, HIGH);
+        digitalWrite(P2_LED_PIN, LOW);
+      }
     }
     if (mode == 4)
     {
+      oled.clear();
       setDisplayLine(0, "          ");
       setDisplayLine(1, "  P1 WIN  ");
       setDisplayLine(2, "          ");
       setDisplayLine(3, "          ");
+      //victoryMusic();
+      mode = 0;
+      oled.clear();
     }
     else if (mode == 5)
     {
+      oled.clear();
       setDisplayLine(0, "          ");
       setDisplayLine(1, "  P2 WIN  ");
       setDisplayLine(2, "          ");
       setDisplayLine(3, "          ");
+      //victoryMusic();
+      mode = 0;
+      oled.clear();
     }
   }
   
   t1 = millis();
-  int t0 = (t1 - t0);
+  int td = (t1 - t0);
   // only delay if loop take less than delay
-  if (t0 < DELAY)
-    delay(DELAY - t0);
+  if (td < DELAY)
+    delay(DELAY - td);
 }
 
 bool readBtnState()
@@ -121,65 +180,99 @@ bool readBtnState()
 
 void gameMenu()
 {
-  oled.clear();
-  static unsigned int choice = 1;
+  static int choice = 1;
 
   if (btn_state == 3)
   {
     mode = 5;
     switch(choice)
     {
+      case -3:
+        p1 = 3UL * 60000UL;
+        inc = 2000;
+        break;
+      case -2:
+        p1 = 2UL * 60000UL;
+        inc = 1000;
+        break;
+      case -1:
+        p1 = 1UL * 60000UL;
+        inc = 1000;
+        break;
+      case 0:
+        p1 = 5UL * 60000UL;
+        inc = 0;
+        break;
       case 1:
-        p1 = 1UL * 60UL * 1000UL;
+        p1 = 1UL * 60000UL;
+        inc = 0;
         break;
       case 2:
-        p1 = 3UL * 60UL * 1000UL;
+        p1 = 3UL * 60000UL;
+        inc = 0;
         break;
       case 3:
-        p1 = 5UL * 60UL * 1000UL;
+        p1 = 5UL * 60000UL;
+        inc = 0;
         break;
       case 4:
-        p1 = 10UL * 60UL * 1000UL;
+        p1 = 10UL * 60000UL;
+        inc = 0;
         break;
       case 5:
-        p1 = 20UL * 60UL * 1000UL;
+        p1 = 20UL * 60000UL;
+        inc = 0;
         break;
       case 6:
-        p1 = 30UL * 60UL * 1000UL;
+        p1 = 30UL * 60000UL;
+        inc = 0;
         break;
     }
     p2 = p1;
     return;
   }
+  
   else if(btn_state == 2)
     choice++;
   else if (btn_state == 1)
     choice--;
 
-  if (choice <= 0)
-    choice = 1;
+  if (choice < -3)
+    choice = -3;
   else if (choice > 6)
     choice = 6;
   
   switch(choice)
   {
+    case -3:
+      setDisplayLine(0, "3 min | 2");
+      break;
+    case -2:
+      setDisplayLine(0, "2 min | 1");
+      break;
+    case -1:
+      setDisplayLine(0, "1 min | 1");
+      break;
+    case 0:
+      setDisplayLine(0, "5 min diff");
+      break;
     case 1:
-      setDisplayLine(0, "2 x 1 min ");
+      setDisplayLine(0, "1 min ");
       break;
     case 2:
-      setDisplayLine(0, "2 x 3 min ");
+      setDisplayLine(0, "3 min ");
       break;
     case 3:
-      setDisplayLine(0, "2 x 5 min ");
+      setDisplayLine(0, "5 min ");
       break;
     case 4:
-      setDisplayLine(0, "2 x 10 min");
+      setDisplayLine(0, "10 min");
       break;
     case 5:
-      setDisplayLine(0, "2 x 20 min");
+      setDisplayLine(0, "20 min");
       break;
     case 6:
-      setDisplayLine(0, "2 x 30 min");
+      setDisplayLine(0, "30 min");
       break;
   }
 }
