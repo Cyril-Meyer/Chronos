8e4b46a42394c1248bd9bd43cb7f5795ab9c57e8
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-06-15 18:16:34 +0200

    add Music module from robsoncouto/arduino-songs

---
+++ Music.ino
@@ -0,0 +1,61 @@
+/*
+  based on the original code from : https://github.com/robsoncouto/arduino-songs
+*/
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define REST      0
+
+int tempo = 240;
+int buzzer = 5;
+
+int melody[] = {
+  NOTE_DS4,8, NOTE_E4,-4, REST,8, NOTE_FS4,8, NOTE_G4,-4, REST,8, NOTE_DS4,8,
+  NOTE_E4,-8, NOTE_FS4,8,  NOTE_G4,-8, NOTE_C5,8, NOTE_B4,-8, NOTE_E4,8, NOTE_G4,-8, NOTE_B4,8,   
+  NOTE_AS4,2, NOTE_A4,-16, NOTE_G4,-16, NOTE_E4,-16, NOTE_D4,-16, NOTE_E4,2
+};
+
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+int wholenote = (60000 * 4) / tempo;
+int divider = 0, noteDuration = 0;
+
+void victoryMusic()
+{
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2)
+  {
+    divider = melody[thisNote + 1];
+    if (divider > 0)
+    {
+      noteDuration = (wholenote) / divider;
+    } 
+    else if (divider < 0)
+    {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;
+    }
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+    delay(noteDuration);
+    noTone(buzzer);
+  }
+}
